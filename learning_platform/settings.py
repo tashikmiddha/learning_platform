be14b4041223536530134ecc09c1b33f4144aa3f
tashikmiddha <136367718+tashikmiddha@users.noreply.github.com>
2025-08-29 04:28:10 +0530

    Update settings.py

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -89,11 +89,12 @@ WSGI_APPLICATION = 'learning_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://username:password@host:port/dbname'
+    )
 }
 
 
